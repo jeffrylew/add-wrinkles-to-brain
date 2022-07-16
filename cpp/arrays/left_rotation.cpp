df076f85e151c94620bb3076e95bb04eaaae0d66
Jeffry <jeffryklew@gmail.com>
2022-07-16 16:19:35 -0400

    Fix gtest vector comparison in arrays/left_rotation.cpp

--- cpp/arrays/left_rotation.cpp
+++ cpp/arrays/left_rotation.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <vector>
 
 static std::vector<int> rotLeft(std::vector<int> a, int d)
@@ -12,23 +13,37 @@ static std::vector<int> rotLeft(std::vector<int> a, int d)
 
 // Test case 0
 TEST(RotLeftTest, TestCase0) {
-    EXPECT_EQ({5, 1, 2, 3, 4}, rotLeft({1, 2, 3, 4, 5}, 4));
+    std::vector<int> expected_vec {5, 1, 2, 3, 4};
+    const auto       rotated_vec = rotLeft({1, 2, 3, 4, 5}, 4);
+
+    EXPECT_TRUE(std::equal(expected_vec.cbegin(),
+                           expected_vec.cend(),
+                           rotated_vec.cbegin()));
 }
 
 // Test case 1
 TEST(RotLeftTest, TestCase1) {
-    EXPECT_EQ(
-        {77, 97, 58, 1, 86, 58, 26, 10, 86, 51,
-         41, 73, 89, 7, 10, 1, 59, 58, 84, 77},
+    std::vector<int> expected_vec {77, 97, 58, 1, 86, 58, 26, 10, 86, 51,
+                                   41, 73, 89, 7, 10, 1, 59, 58, 84, 77};
+
+    const auto rotated_vec =
         rotLeft({41, 73, 89, 7, 10, 1, 59, 58, 84, 77,
-                77, 97, 58, 1, 86, 58, 26, 10, 86, 51}, 10));
+                 77, 97, 58, 1, 86, 58, 26, 10, 86, 51}, 10);
+
+    EXPECT_TRUE(std::equal(expected_vec.cbegin(),
+                           expected_vec.cend(),
+                           rotated_vec.cbegin()));
 }
 
 // Test case 10
 TEST(RotLeftTest, TestCase10) {
-    EXPECT_EQ(
-        {87, 97, 33, 47, 70, 37, 8, 53,
-         13, 93, 71, 72, 51, 100, 60},
-        rotLeft({33, 47, 70, 37, 8, 53, 13, 93,
-                 71, 72, 51, 100, 60, 87, 97}, 13));
+    std::vector<int> expected_vec {87, 97, 33, 47, 70, 37, 8, 53,
+                                   13, 93, 71, 72, 51, 100, 60};
+
+    const auto rotated_vec = rotLeft({33, 47, 70, 37, 8, 53, 13, 93,
+                                      71, 72, 51, 100, 60, 87, 97}, 13);
+
+    EXPECT_TRUE(std::equal(expected_vec.cbegin(),
+                           expected_vec.cend(),
+                           rotated_vec.cbegin()));
 }
