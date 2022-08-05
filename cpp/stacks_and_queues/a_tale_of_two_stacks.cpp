d1162d9049099c0267ae601c00f4c994f1ec5e53
Jeffry <jeffryklew@gmail.com>
2022-08-04 22:32:06 -0400

    Implement discussion solution in stacks_and_queues/a_tale_of_two_stacks.cpp

--- cpp/stacks_and_queues/a_tale_of_two_stacks.cpp
+++ cpp/stacks_and_queues/a_tale_of_two_stacks.cpp
@@ -53,13 +53,72 @@ public:
     {
         return stack_oldest_on_top.top();
     }
-};
+
+}; // class MyQueueFirstAttempt : ...
+
+//! @brief Solution with hint from discussion section
+class MyQueueDiscussionSolution : public MyQueue
+{  
+public:
+    MyQueueDiscussionSolution()           = default;
+    ~MyQueueDiscussionSolution() override = default;
+
+    void push(int x) override
+    {
+        // Add newest item
+        stack_newest_on_top.emplace(x);
+    }
+
+    void pop() override
+    {
+        if (stack_oldest_on_top.empty())
+        {
+            // Move newest items to bottom of stack_oldest_on_top
+            while (not stack_newest_on_top.empty())
+            {
+                stack_oldest_on_top.emplace(stack_newest_on_top.top());
+                stack_newest_on_top.pop();
+            }
+        }
+        
+        // Pop oldest item
+        stack_oldest_on_top.pop();
+    }
+
+    int front() override
+    {
+        if (stack_oldest_on_top.empty())
+        {
+            // Move newest items to bottom of stack_oldest_on_top
+            while (not stack_newest_on_top.empty())
+            {
+                stack_oldest_on_top.emplace(stack_newest_on_top.top());
+                stack_newest_on_top.pop();
+            }
+        }
+        return stack_oldest_on_top.top();
+    }
+
+}; // class MyQueueDiscussionSolution : ...
 
 // Test case 0
 TEST(MyQueueTest, TestCase0) {
     std::unique_ptr<MyQueue> q {nullptr};
     
     q = std::make_unique<MyQueueFirstAttempt>();
+    q->push(42);
+    q->pop();
+    q->push(14);
+    EXPECT_EQ(14, q->front());
+    q->push(28);
+    EXPECT_EQ(14, q->front());
+    q->push(60);
+    q->push(78);
+    q->pop();
+    q->pop();
+    q.reset();
+
+    q = std::make_unique<MyQueueDiscussionSolution>();
     q->push(42);
     q->pop();
     q->push(14);
@@ -85,6 +144,17 @@ TEST(MyQueueTest, TestCase16) {
     EXPECT_EQ(17, q->front());
     q->pop();
     EXPECT_EQ(25, q->front());
+    q.reset();
+
+    q = std::make_unique<MyQueueDiscussionSolution>();
+    q->push(15);
+    q->push(17);
+    EXPECT_EQ(15, q->front());
+    q->push(25);
+    q->pop();
+    EXPECT_EQ(17, q->front());
+    q->pop();
+    EXPECT_EQ(25, q->front());
 }
 
 // Test case 17
@@ -92,6 +162,17 @@ TEST(MyQueueTest, TestCase17) {
     std::unique_ptr<MyQueue> q {nullptr};
     
     q = std::make_unique<MyQueueFirstAttempt>();
+    q->push(12);
+    q->push(14);
+    EXPECT_EQ(12, q->front());
+    q->pop();
+    EXPECT_EQ(14, q->front());
+    q->push(71);
+    q->push(63);
+    EXPECT_EQ(14, q->front());
+    q.reset();
+
+    q = std::make_unique<MyQueueDiscussionSolution>();
     q->push(12);
     q->push(14);
     EXPECT_EQ(12, q->front());
