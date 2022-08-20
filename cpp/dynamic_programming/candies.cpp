b7d432e0a7f13d08d9de6474e588d9f3584c2957
Jeffry <jeffryklew@gmail.com>
2022-08-20 15:16:20 -0400

    Implement LC O(1) discussion solution in dynamic_programming/candies.cpp

--- cpp/dynamic_programming/candies.cpp
+++ cpp/dynamic_programming/candies.cpp
@@ -88,6 +88,10 @@ static long candiesLCDiscussionSolution_O_n(int n, std::vector<int> arr)
     //!          A-simple-solution
     //!
     //!          O(n) time and O(n) space
+    //!
+    //!          This is a greedy approach according to LC:
+    //!          https://leetcode.com/problems/candy/discuss/1300022/
+    //!          Candy-Distribution-oror-C%2B%2B-oror-99-faster-oror-explained
 
     //! Less than two children case
     if (n <= 1)
@@ -118,17 +122,95 @@ static long candiesLCDiscussionSolution_O_n(int n, std::vector<int> arr)
                                                 candies_per_child[i - 1]);
         }
     }
-    
+
     return std::accumulate(candies_per_child.cbegin(),
                            candies_per_child.cend(),
                            0L);
 
 } // static long candiesLCDiscussionSolution_O_n( ...
 
+//! @brief LC discussion solution with O(1) space
+//! @param[in] n   Number of children in class
+//! @param[in] arr Vector of student ratings
+//! @return Minimum number of candies to buy
+static long candiesLCDiscussionSolution_O_1(int n, std::vector<int> arr)
+{
+    //! @details https://leetcode.com/problems/candy/discuss/135698/
+    //!          Simple-solution-with-one-pass-using-O(1)-space
+    //!
+    //!          O(n) time and O(1) space
+    //!
+    //!          Example child ratings:   0 1 9 8 7 6
+    //!          Distribution of candies: 1 2 4 3 2 1
+    //!          Candies from this soln:  1 2 3 1 2 4
+
+    //! Case if there are no children
+    if (n == 0)
+    {
+        return 0L;
+    }
+
+    //! Minimum number of candies, initialized to 1 (for one child case)
+    long min_candies {1L};
+
+    //! Track number of continuous rating increases and decreases
+    //! (Excludes starting child)
+    long up {};
+    long down {};
+
+    //! Track peak value of "up" before rating decrease
+    long peak {};
+
+    //! Iterate from second child (if there are at least two children)
+    for (int i = 1; i < n; ++i)
+    {
+        if (arr[i - 1] < arr[i])
+        {
+            //! Rating is increasing
+            down = 0L;
+            peak = ++up;
+
+            //! Addition of 1L accounts for starting child
+            min_candies += 1L + up;
+        }
+        else if (arr[i - 1] > arr[i])
+        {
+            //! Rating is decreasing
+            up = 0L;
+            ++down;
+            min_candies += down;
+
+            //! Give child at peak point one additional candy
+            //! since there are more down than up points
+            //! Technically this is not happening at the peak child
+            //! but the result of min_candies is the same
+            if (down > peak)
+            {
+                ++min_candies;
+            }
+        }
+        else
+        {
+            //! arr[i - 1] == arr[i]
+            peak = 0L;
+            up   = 0L;
+            down = 0L;
+
+            //! Child i receives one candy
+            ++min_candies;
+        }
+
+    } // for (int i = 1; i < n; ++i)
+
+    return min_candies;
+
+} // static long candiesLCDiscussionSolution_O_1( ...
+
 // Test case 0
 TEST(CandiesTest, TestCase0) {
     EXPECT_EQ(4L, candiesFirstAttempt(3, {1, 2, 2}));
     EXPECT_EQ(4L, candiesLCDiscussionSolution_O_n(3, {1, 2, 2}));
+    EXPECT_EQ(4L, candiesLCDiscussionSolution_O_1(3, {1, 2, 2}));
 }
 
 // Test case 10
@@ -138,6 +220,7 @@ TEST(CandiesTest, TestCase10) {
     
     EXPECT_EQ(19L, candiesFirstAttempt(10, arr));
     EXPECT_EQ(19L, candiesLCDiscussionSolution_O_n(10, arr));
+    EXPECT_EQ(19L, candiesLCDiscussionSolution_O_1(10, arr));
 }
 
 // Test case 16
@@ -147,4 +230,5 @@ TEST(CandiesTest, TestCase16) {
     
     EXPECT_EQ(12L, candiesFirstAttempt(8, arr));
     EXPECT_EQ(12L, candiesLCDiscussionSolution_O_n(8, arr));
+    EXPECT_EQ(12L, candiesLCDiscussionSolution_O_1(8, arr));
 }
