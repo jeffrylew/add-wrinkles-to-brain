ae3044474d291e81ce60e40cc9abc16cabd2bac2
Jeffry <jeffryklew@gmail.com>
2022-08-27 14:58:36 -0400

    Implement first discussion solution in dynamic_programming/abbreviation.cpp

--- cpp/dynamic_programming/abbreviation.cpp
+++ cpp/dynamic_programming/abbreviation.cpp
@@ -2,7 +2,9 @@
 
 #include <algorithm>
 #include <iterator>
+#include <locale>
 #include <string>
+#include <vector>
 
 //! @brief First attempt solution fails Test cases 6-14
 static std::string abbreviationFirstAttempt(std::string a, std::string b)
@@ -80,17 +82,113 @@ static std::string abbreviationFirstAttempt(std::string a, std::string b)
 } // static std::string abbreviationFirstAttempt( ...
 
 //! @brief Solution from discussion section
-static std::string abbreviationDiscussionSolution(std::string a, std::string b)
+//! @param[in] a String to modify
+//! @param[in] b String to match
+//! @return "YES" if a can be modified to match b, else "NO"
+static std::string abbreviationDiscussionSolution1(std::string a, std::string b)
 {
-    //! @todo Turn hints into code
+    //! @details https://www.hackerrank.com/challenges/abbr/forum
 
-} // static std::string abbreviationDiscussionSolution( ...
+    //! Addition of 1ULL accounts for empty substrings
+    const auto dp_len_a = a.size() + 1ULL;
+    const auto dp_len_b = b.size() + 1ULL;
+    
+    //! dp tracks whether the substring of b is a
+    //! subsequence of substring of a. Values are
+    //! initialized to false
+    //!
+    //! dp_len_a rows, assume this is y-axis
+    //! dp_len_b cols, assume this is x-axis
+    std::vector<std::vector<bool>> dp(
+        dp_len_b, std::vector<bool>(dp_len_a, false));
+    
+    //! Initialize case when string a (to modify)
+    //! and string b (to match) are both empty
+    dp[0][0] = true;
+    
+    //! Index col = 0 represents empty b substring
+    //! Index row = 0 represents empty a substring
+    
+    //! Use classic C++ locale
+    const std::locale loc {};
+
+    for (std::size_t col = 0; col < dp_len_b; ++col)
+    {
+        //! This loop could start with row = 1 and checks
+        //! on (row > 0) could be removed below
+        for (std::size_t row = 0; row < dp_len_a; ++row)
+        {
+            //! Case when letters remain in string b (to match) but
+            //! there are no letters left in string a (to modify):
+            //! col > 0 and row == 0 already initialized to false
+            
+            if (col == 0 && row > 0)
+            {
+                //! Current character in a
+                const auto char_a = a[row - 1];
+                
+                //! Substring of b (to match) is empty but
+                //! there are chars left in a (to modify)
+                //! Current char in a needs to be lowercase AND
+                //! prior substring of a is an abbreviation
+                dp[col][row] = std::islower(char_a, loc) && dp[col][row - 1];
+            }
+            else if (col > 0 && row > 0)
+            {
+                //! Case when substrings of a and b are not empty
+                
+                const auto char_a = a[row - 1];
+                const auto char_b = b[col - 1];
+                
+                if (char_a == char_b)
+                {
+                    //! Char in a exactly matches char in b
+                    //! (both lowercase or both uppercase)
+                    //! Current substring b is a subsequence
+                    //! of substring a if the prior substrings
+                    //! were abbreviations
+                    dp[col][row] = dp[col - 1][row - 1];
+                }
+                else if (std::toupper(char_a, loc) == char_b)
+                {
+                    //! Char in a is lowercase version of char in b
+                    //! Check if prior substrings were abbreviations
+                    //! or if substring b is still an abbreviation of
+                    //! substring a if this lowercase char is dropped
+                    dp[col][row] = dp[col - 1][row - 1] || dp[col][row - 1];
+                }
+                else if (not (
+                    std::isupper(char_a, loc) && std::isupper(char_b, loc)))
+                {
+                    //! The first line below should result in false:
+                    //!
+                    //! (Char in a uppercase, char in b lowercase) OR
+                    //! (char in a lowercase, char in b uppercase BUT
+                    //! char in a not same letter as char in b) OR
+                    //! (char in a lowercase, char in b lowercase BUT
+                    //! char in a not same letter as char in b)
+                    
+                    dp[col][row] = dp[col][row - 1];
+                }
+                
+                //! Char in a uppercase, char in b uppercase but they
+                //! aren't the same letter - defaults to false
+                
+            } // else if (col > 0 && row > 0)
+            
+        } // for (std::size_t row = 0; ...
+        
+    } // for (std::size_t col = 0; ...
+    
+    return dp[b.size()][a.size()] ? "YES" : "NO";
+
+} // static std::string abbreviationDiscussionSolution1( ...
 
 // Test case 0
 TEST(AbbreviationTest, TestCase0) {
     EXPECT_EQ("YES", abbreviationFirstAttempt("daBcd", "ABC"));
 
-    // EXPECT_EQ("YES", abbreviationDiscussionSolution("daBcd", "ABC"));
+    EXPECT_EQ("YES", abbreviationDiscussionSolution1("daBcd", "ABC"));
 }
 
 // Test case 1
@@ -106,18 +204,16 @@ TEST(AbbreviationTest, TestCase1) {
     EXPECT_EQ("NO", abbreviationFirstAttempt("sYOCa", "YOCN"));
     EXPECT_EQ("NO", abbreviationFirstAttempt("JHMWY", "HUVPW"));
 
-    /*
-    EXPECT_EQ("YES", abbreviationDiscussionSolution("Pi", "P"));
-    EXPECT_EQ("NO", abbreviationDiscussionSolution("AfPZN", "APZNC"));
-    EXPECT_EQ("NO", abbreviationDiscussionSolution("LDJAN", "LJJM"));
-    EXPECT_EQ("YES", abbreviationDiscussionSolution("UMKFW", "UMKFW"));
-    EXPECT_EQ("NO", abbreviationDiscussionSolution("KXzQ", "K"));
-    EXPECT_EQ("YES", abbreviationDiscussionSolution("LIT", "LIT"));
-    EXPECT_EQ("YES", abbreviationDiscussionSolution("QYCH", "QYCH"));
-    EXPECT_EQ("YES", abbreviationDiscussionSolution("DFIQG", "DFIQG"));
-    EXPECT_EQ("NO", abbreviationDiscussionSolution("sYOCa", "YOCN"));
-    EXPECT_EQ("NO", abbreviationDiscussionSolution("JHMWY", "HUVPW"));
-    */
+    EXPECT_EQ("YES", abbreviationDiscussionSolution1("Pi", "P"));
+    EXPECT_EQ("NO", abbreviationDiscussionSolution1("AfPZN", "APZNC"));
+    EXPECT_EQ("NO", abbreviationDiscussionSolution1("LDJAN", "LJJM"));
+    EXPECT_EQ("YES", abbreviationDiscussionSolution1("UMKFW", "UMKFW"));
+    EXPECT_EQ("NO", abbreviationDiscussionSolution1("KXzQ", "K"));
+    EXPECT_EQ("YES", abbreviationDiscussionSolution1("LIT", "LIT"));
+    EXPECT_EQ("YES", abbreviationDiscussionSolution1("QYCH", "QYCH"));
+    EXPECT_EQ("YES", abbreviationDiscussionSolution1("DFIQG", "DFIQG"));
+    EXPECT_EQ("NO", abbreviationDiscussionSolution1("sYOCa", "YOCN"));
+    EXPECT_EQ("NO", abbreviationDiscussionSolution1("JHMWY", "HUVPW"));
 }
 
 // Test case 15
@@ -126,9 +222,7 @@ TEST(AbbreviationTest, TestCase15) {
     EXPECT_EQ("NO", abbreviationFirstAttempt("beFgH", "EFG"));
     EXPECT_EQ("YES", abbreviationFirstAttempt("beFgH", "EFH"));
 
-    /*
-    EXPECT_EQ("NO", abbreviationDiscussionSolution("AbCdE", "AFE"));
-    EXPECT_EQ("NO", abbreviationDiscussionSolution("beFgH", "EFG"));
-    EXPECT_EQ("YES", abbreviationDiscussionSolution("beFgH", "EFH"));
-    */
+    EXPECT_EQ("NO", abbreviationDiscussionSolution1("AbCdE", "AFE"));
+    EXPECT_EQ("NO", abbreviationDiscussionSolution1("beFgH", "EFG"));
+    EXPECT_EQ("YES", abbreviationDiscussionSolution1("beFgH", "EFH"));
 }
