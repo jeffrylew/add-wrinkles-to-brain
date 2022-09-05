60d7b1d427f17bb34ea6bcab701833b01ebc7898
Jeffry <jeffryklew@gmail.com>
2022-09-05 09:35:30 -0400

    Implement top-down merge sort from Wikipedia in sorting/merge_sort.cpp

--- cpp/sorting/merge_sort.cpp
+++ cpp/sorting/merge_sort.cpp
@@ -3,9 +3,20 @@
 #include <algorithm>
 #include <vector>
 
-//! Forward declaration
+//! Forward declarations
 std::vector<int> combineSortedVectorsIC(const std::vector<int>& vec1,
                                         const std::vector<int>& vec2);
+
+void TopDownSplitMerge(std::vector<int>& B,
+                       int               iBegin,
+                       int               iEnd,
+                       std::vector<int>& A);
+
+void TopDownMerge(const std::vector<int>& A,
+                  int                     iBegin,
+                  int                     iMid,
+                  int                     iEnd,
+                  std::vector<int>&       B);
 
 //! @brief Merge sort from https://www.interviewcake.com/concept/cpp/merge-sort
 static std::vector<int> mergeSortIC(const std::vector<int>& vec)
@@ -76,10 +87,80 @@ std::vector<int> combineSortedVectorsIC(const std::vector<int>& vec1,
 
 } // std::vector<int> combineSortedVectorsIC( ...
 
-TEST(MergeSortTest, TestInterviewCake) {
+//! @brief Top-down merge sort from https://en.wikipedia.org/wiki/Merge_sort
+//! @param[in,out] A Reference to vector of items to sort
+static void TopDownMergeSort(std::vector<int>& A)
+{
+    //! Vector B is a work vector, one-time copy of elements from A to B
+    auto B = A;
+
+    //! Sort data from B into A
+    TopDownSplitMerge(B, 0, A.size(), A);
+}
+
+//! Split A into two lists. Sort both lists into B, merge both lists from B to A
+//! iBegin is inclusive; iEnd is exclusive (A[iEnd] is not in the set).
+void TopDownSplitMerge(std::vector<int>& B,
+                       int               iBegin,
+                       int               iEnd,
+                       std::vector<int>& A)
+{
+    //! Base case: Single element vector is already sorted
+    if (iEnd - iBegin <= 1)
+    {
+        return;
+    }
+    
+    //! Split the list into halves
+    auto iMid = (iEnd + iBegin) / 2;
+
+    //! Recursively sort both halves from A into B
+    TopDownSplitMerge(A, iBegin, iMid, B);
+    TopDownSplitMerge(A, iMid, iEnd, B);
+
+    //! Merge resulting halves from B into A
+    TopDownMerge(B, iBegin, iMid, iEnd, A);
+
+} // void TopDownSplitMerge( ...
+
+//! Left source half is  A[iBegin:iMid - 1]
+//! Right source half is A[iMid:iEnd - 1]
+//! Result is            B[iBegin:iEnd - 1]
+void TopDownMerge(const std::vector<int>& A,
+                  int                     iBegin,
+                  int                     iMid,
+                  int                     iEnd,
+                  std::vector<int>&       B)
+{
+    auto LHS_idx = iBegin;
+    auto RHS_idx = iMid;
+
+    //! While there are elements in the LHS or RHS lists...
+    for (int k = iBegin; k < iEnd; ++k)
+    {
+        //! If first element in LHS list exists and is <= first element in RHS
+        if (LHS_idx < iMid && (RHS_idx >= iEnd || A[LHS_idx] <= A[RHS_idx]))
+        {
+            B[k] = A[LHS_idx++];
+        }
+        else
+        {
+            B[k] = A[RHS_idx++];
+        }
+    }
+
+} // void TopDownMerge( ...
+
+TEST(MergeSortTest, TestSimple) {
     const std::vector<int> expected {1, 2, 3, 4, 7, 8, 9};
 
     EXPECT_TRUE(std::equal(expected.cbegin(),
                            expected.cend(),
                            mergeSortIC({8, 3, 2, 7, 9, 1, 4}).cbegin()));
+    
+    std::vector<int> input {8, 3, 2, 7, 9, 1, 4};
+    TopDownMergeSort(input);
+    EXPECT_TRUE(std::equal(expected.cbegin(),
+                           expected.cend(),
+                           input.cbegin()));
 }
