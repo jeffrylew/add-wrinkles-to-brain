492d43db69f90a68b8d993649775119ea0330553
Jeffry <jeffryklew@gmail.com>
2022-09-11 11:10:39 -0400

    Add first attempt solution to graphs/find_the_nearest_clone.cpp

--- cpp/graphs/find_the_nearest_clone.cpp
+++ cpp/graphs/find_the_nearest_clone.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <queue>
 #include <vector>
 
-//! @brief First attempt solution
+//! @brief First attempt solution passes all tests
 //! @param[in] graph_nodes Number of nodes
 //! @param[in] graph_from  Vector of start nodes for each edge
 //! @param[in] graph_to    Vector of end nodes for each edge
@@ -15,7 +17,94 @@ static int findShortestFirstAttempt(int               graph_nodes,
                                     std::vector<long> ids,
                                     int               val)
 {
+    //! If color val not present in ids at least twice, return -1
+    if (std::count(ids.cbegin(), ids.cend(), static_cast<long>(val)) < 2)
+    {
+        return -1;
+    }
+
+    //! Store connected edges in undirected graph
+    std::vector<std::vector<int>> connected_nodes(graph_nodes);
+    if (graph_from.size() == graph_to.size())
+    {
+        for (std::size_t i = 0; i < graph_from.size(); ++i)
+        {
+            //! Add graph nodes with 0-based indexing
+            connected_nodes[graph_from[i] - 1].emplace_back(graph_to[i] - 1);
+            connected_nodes[graph_to[i] - 1].emplace_back(graph_from[i] - 1);
+        }
+    }
+
+    //! Find starting node, i.e. first node with color val
+    int start_node {};
     
+    for (int i = 0; i < static_cast<int>(ids.size()); ++i)
+    {
+        if (ids[i] == static_cast<long>(val))
+        {
+            start_node = i;
+            break;
+        }
+    }
+    
+    /*
+     * Alternatively, can use std::find_if
+     *
+    auto start_node_it = std::find_if(ids.cbegin(),
+                                      ids.cend(),
+                                      static_cast<long>(val));
+    if (start_node_it != ids.cend())
+    {
+        start_node = std::distance(ids.cbegin(), start_node_it) - 1;
+    }
+     */
+
+    //! Queue of nodes to process, initialize with start_node
+    std::queue<int> nodes_todo {};
+    nodes_todo.push(start_node);
+
+    //! Output vector of shortest distances, O(n = graph_nodes + 1) space
+    //! Initialized with -1 except at start_node index, which is 0
+    std::vector<int> shortest_distances(graph_nodes, -1);
+    shortest_distances[start_node] = 0;
+
+    while (not nodes_todo.empty())
+    {
+        const auto curr_node = nodes_todo.front();
+        nodes_todo.pop();
+
+        //! If connected nodes of curr_node have not been processed yet
+        //! (distance == -1) then add each to queue and update distance
+        for (const auto connected_node : connected_nodes[curr_node])
+        {
+            if (shortest_distances[connected_node] != -1)
+            {
+                //! Connected node has already been processed since distance is
+                //! not -1 so skip it. With BFS, the node will always be visited
+                //! at the earliest time. This is O(1) space instead of O(n),
+                //! which a solution using a set of visited nodes would require
+                continue;
+            }
+            
+            //! Update distance of connected node
+            shortest_distances[connected_node] =
+                shortest_distances[curr_node] + 1;
+
+            //! If connected_node has color matching val, return
+            if (ids[connected_node] == static_cast<long>(val))
+            {
+                return shortest_distances[connected_node];
+            }
+
+            //! Otherwise add connected node to queue
+            nodes_todo.push(connected_node);
+
+        } // for (const auto connected_node : ...
+        
+    } // while (not nodes_todo.empty())
+
+    //! A node with matching color was not connected at this point
+    return -1;
 
 } // static int findShortestFirstAttempt( ...
 
