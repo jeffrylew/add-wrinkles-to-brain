ede4cfd393561251da78c60907b9701748b84480
Jeffry <jeffryklew@gmail.com>
2022-09-20 20:26:26 -0400

    Implement discussion solution in search/maximum_subarray_sum.cpp

--- cpp/search/maximum_subarray_sum.cpp
+++ cpp/search/maximum_subarray_sum.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <limits>
+#include <set>
 #include <vector>
 
 //! @brief First attempt solution times out for Test cases 6-16
@@ -55,17 +58,55 @@ static long maximumSumFirstAttempt(std::vector<long> a, long m)
 
 } // static long maximumSumFirstAttempt( ...
 
+//! @brief Solution from HR discussion section
+//! @param[in] a Array of n elements
+//! @param[in] m The modulo divisor
+//! @return Maximum subarray sum modulo m
+static long maximumSumDiscussionSolution(std::vector<long> a, long m)
+{
+    //! @details
+    //!     https://www.hackerrank.com/challenges/maximum-subarray-sum/forum
+
+    long sum {};
+    long max {std::numeric_limits<long>::min()};
+    long result {std::numeric_limits<long>::max()};
+    
+    std::set<long> s {};
+    
+    for (auto& ele : a)
+    {
+        sum = (sum + ele) % m;
+        ele = sum;
+        max = std::max(max, sum);
+    }
+    
+    for (auto x : a)
+    {
+        auto p = s.insert(x);
+        if (++p.first != s.end())
+        {
+            result = std::min(*p.first - x, result);
+        }
+    }
+    
+    return std::max(max, m - result);
+
+} // static long maximumSumDiscussionSolution( ...
+
 // Test case 0
 TEST(MaximumSumTest, TestCase0) {
     EXPECT_EQ(6L, maximumSumFirstAttempt({3L, 3L, 9L, 9L, 5L}, 7L));
+    EXPECT_EQ(6L, maximumSumDiscussionSolution({3L, 3L, 9L, 9L, 5L}, 7L));
 }
 
 // Test case 17
 TEST(MaximumSumTest, TestCase17) {
     EXPECT_EQ(1L, maximumSumFirstAttempt({1L, 2L, 3L}, 2L));
+    EXPECT_EQ(1L, maximumSumDiscussionSolution({1L, 2L, 3L}, 2L));
 }
 
 // Test case 18
 TEST(MaximumSumTest, TestCase18) {
     EXPECT_EQ(4L, maximumSumFirstAttempt({1L, 5L, 9L}, 5L));
+    EXPECT_EQ(4L, maximumSumDiscussionSolution({1L, 5L, 9L}, 5L));
 }
