708ef24a5159c4e12803844a4a96de9e2a678512
Jeffry <jeffryklew@gmail.com>
2022-09-24 15:38:23 -0400

    Add comments from YouTube video to search/maximum_subarray_sum.cpp

--- cpp/search/maximum_subarray_sum.cpp
+++ cpp/search/maximum_subarray_sum.cpp
@@ -158,6 +158,53 @@ static long maximumSumDiscussionSolution(std::vector<long> a, long m)
     //!    prefix[i] = 2, 3, 5, 5
     //! where j = 3 > i = 0, prefix[i = 0] - prefix[j = 3] = 3 - 2 = 1
     //! and m - 1 = 7 - 1 = 6
+    //!
+    //! Additional explanation from https://www.youtube.com/watch?v=u_ft5jCDZXk:
+    //!     a[i] % m = (a[i] + m) % m from (a[i] % m) + (m % m = 0)
+    //! The sub-array sum modulo m from index i + 1 to j is equal to
+    //!     sum_mod_m[i + 1:j] = (prefix[j] - prefix[i] + m) % m
+    //! where m is added to (prefix[j] - prefix[i]), which could be negative.
+    //! This uses the first property above, sum % m = (sum + m) % m.
+    //!
+    //! However, calculating the sub-array sums for all combinations results in
+    //! O(n^2) time complexity. Comparisons where prefix[i] <= prefix[j] cannot
+    //! increase sum_mod_m since prefix[j] is being decreased. We only want to
+    //! focus on cases where prefix[i] > prefix[j] for index i < j. Furthermore,
+    //! the smallest value of prefix[i] greater than prefix[j] will maximize
+    //! sum_mod_m. This is the reason for maintaining a sorted data structure of
+    //! prefix sums to quickly determine the minimum difference between two
+    //! adjacent elements when the first index is larger than the second (i < j)
+    //!
+    //! A sorted vector is contiguous and cache friendly. Back insertion is
+    //! amortized O(1) and sorting with std::sort is O(N log(N)) where
+    //! N = (last - first) comparisons. Sorting stability is not needed since we
+    //! only care about differences between non-equal prefix sums. Otherwise,
+    //! std::stable_sort preserves the order of equivalent elements with
+    //! O(N log(N)^2) time complexity for N = (last - first) comparisons.
+    //! However, we need to find the prefix sum we inserted, which is O(N) for
+    //! an unsorted vector. This causes the overall time complexity to be O(N^2)
+    //! But since our vector is sorted, binary search can find the element in
+    //! O(log N) average time through std::lower_bound. The overall time
+    //! complexity is then O(N log(N)).
+    //!
+    //! If a BST were used, the find and insertion operations are O(log N) (the
+    //! height of the tree). But if the tree becomes skewed then they are
+    //! performed with O(N) time complexity, causing the overall complexity to
+    //! be O(N^2).
+    //!
+    //! If an AVL/Red-Black Tree/self-balancing tree were used (e.g. std::set),
+    //! then the find and insertion operations are O(log N). std::set was used
+    //! in the discussion solution below.
+    //!
+    //!     s.insert(x) // Insert prefix sum at correct location i (if unique)
+    //!     ++p.first   // Get prefix sum immediately larger for index j > i
+    //!                 // The check != s.end() prevents checking cases where
+    //!                 // the prefix sum is monotonically increasing, i.e. when
+    //!                 // prefix[i] <= prefix[j] for i < j since insertion is
+    //!                 // at the end of the set.
+    //!     std::min(*p.first - x, result) // Get min difference for prefix sums
+    //!                                    // when prefix[i] > prefix[j]
+    //!                                    // *p.first - x is a positive value
     for (auto x : a)
     {
         auto p = s.insert(x);
@@ -167,6 +214,10 @@ static long maximumSumDiscussionSolution(std::vector<long> a, long m)
         }
     }
     
+    //! Since result is a positive value, subtract it from m
+    //! Essentially we evaluate the sub-array sum modulo m from index i + 1 to j
+    //!     sum_mod_m[i + 1:j] = m - (prefix[i] - prefix[j]) for i < j and
+    //!     prefix[i] > prefix[j] due to usage of std::set
     return std::max(max, m - result);
 
 } // static long maximumSumDiscussionSolution( ...
