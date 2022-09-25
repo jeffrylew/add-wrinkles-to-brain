43c907e639282dccc5d9881f9951f8c3ca577b61
Jeffry <jeffryklew@gmail.com>
2022-09-25 11:33:24 -0400

    Implement DP solution in search/maximum_subarray.cpp

--- cpp/search/maximum_subarray.cpp
+++ cpp/search/maximum_subarray.cpp
@@ -26,7 +26,35 @@ static int maxSubArray(std::vector<int> nums)
 
 } // static int maxSubArray( ...
 
+//! @brief Dynamic programming solution from LC
+//! @param[in] nums Vector containing at least one number
+//! @return Largest sum of contiguous subarray
+static int maxSubArrayDP(std::vector<int> nums)
+{
+    //! @details https://leetcode.com/problems/maximum-subarray/discuss/20193/
+    //!          DP-solution-and-some-thoughts
+    //!
+    //! Idea using O(n) space (less efficient than Kadane's Algorithm) is
+    //!     maxSubArrayDP(nums, i) = nums[i] + (maxSubArrayDP(nums, i - 1) > 0)
+    //!         ? maxSubArrayDP(nums, i - 1) : 0;
+
+    std::vector<int> dp(nums.size());
+    dp[0] = nums.front();
+
+    int max = dp[0];
+
+    for (int i = 1; i < static_cast<int>(nums.size()); ++i)
+    {
+        dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+        max   = std::max(max, dp[i]);
+    }
+    
+    return max;
+
+} // static int maxSubArrayDP( ...
+
 // LC Test case
 TEST(MaxSubArrayTest, LCTest) {
     EXPECT_EQ(6, maxSubArray({-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    EXPECT_EQ(6, maxSubArrayDP({-2, 1, -3, 4, -1, 2, 1, -5, 4}));
 }
