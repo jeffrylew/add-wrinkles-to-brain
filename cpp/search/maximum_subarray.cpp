5c908e50de6a4816c4336a619f0ada322e6e0faf
Jeffry <jeffryklew@gmail.com>
2022-09-26 21:29:54 -0400

    Implement divide and conquer solution in search/maximum_subarray.cpp

--- cpp/search/maximum_subarray.cpp
+++ cpp/search/maximum_subarray.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <limits>
 #include <vector>
 
 //! @brief From LC, find contiguous subarray which has largest sum
@@ -53,8 +54,77 @@ static int maxSubArrayDP(std::vector<int> nums)
 
 } // static int maxSubArrayDP( ...
 
+//! @brief Helper function for divide and conquer solution (recursive)
+//! @param[in] nums Reference to vector containing at least one number
+//! @param[in] l    Left index to search from
+//! @param[in] r    Right index to search from
+//! @return Largest sum of contiguous subarray
+static int maxSubArrayDC(const std::vector<int>& nums, int l, int r)
+{
+    //! @details Idea is to break nums into halves and find max sub-array sum in
+    //!          them recursively. Tricky part is to handle case where max
+    //!          sub-array sum spans both halves. For this case, use a linear
+    //!          algorithm. Start from middle element and move to right and left
+    //!          ends, recording maximum sums on either side. The max sub-array
+    //!          sum is equal to middle element plus latter two values.
+    //!
+    //!          O(nlog(n)) time complexity. For loop inside recursion can be
+    //!          O(n) in worst case. There are log(n) levels in recursion tree.
+
+    if (l > r)
+    {
+        return std::numeric_limits<int>::min();
+    }
+    
+    //! Middle element index
+    int m {l + (r - l) / 2};
+
+    //! Max sub-array sum on LHS of middle element
+    int ml {};
+
+    //! Max sub-array sum on RHS of middle element
+    int mr {};
+
+    //! Find max sub-array sum on LHS, excluding middle element
+    int lmax = maxSubArrayDC(nums, l, m - 1);
+
+    //! Find max sub-array sum on RHS, excluding middle element
+    int rmax = maxSubArrayDC(nums, m + 1, r);
+
+    //! Handle LHS for max sub-array spanning both halves
+    int sum {};
+    for (int i = m - 1; i >= l; --i)
+    {
+        sum += nums[i];
+        ml = std::max(sum, ml);
+    }
+    
+    //! Handle RHS for max sub-array spanning both halves
+    sum = 0;
+    for (int i = m + 1; i <= r; ++i)
+    {
+        sum += nums[i];
+        mr = std::max(sum, mr);
+    }
+    
+    return std::max(std::max(lmax, rmax), ml + nums[m] + mr);
+
+} // static int maxSubArrayDC( ...
+
+//! @brief Divide and conquer solution from LC
+//! @param[in] nums Vector containing at least one number
+//! @return Largest sum of contiguous subarray
+static int maxSubArrayDC(std::vector<int> nums)
+{
+    //! @details https://leetcode.com/problems/maximum-subarray/discuss/20452/
+    //!          C%2B%2B-DP-and-Divide-and-Conquer
+
+    return maxSubArrayDC(nums, 0, static_cast<int>(nums.size()) - 1);
+}
+
 // LC Test case
 TEST(MaxSubArrayTest, LCTest) {
     EXPECT_EQ(6, maxSubArray({-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     EXPECT_EQ(6, maxSubArrayDP({-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    EXPECT_EQ(6, maxSubArrayDC({-2, 1, -3, 4, -1, 2, 1, -5, 4}));
 }
