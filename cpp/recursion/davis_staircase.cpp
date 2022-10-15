6d4d321df0e100e957607b8b5120d4b364c9220a
Jeffry <jeffryklew@gmail.com>
2022-10-14 21:15:05 -0400

    Implement dynamic programming solution in cpp/recursion/davis_staircase.cpp

--- cpp/recursion/davis_staircase.cpp
+++ cpp/recursion/davis_staircase.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
 #include <unordered_map>
+#include <vector>
 
 //! @brief Recursive helper function for first attempt solution
 //! @param[in]  n                 Number of stairs in the staircase
@@ -70,6 +72,28 @@ static int stepPermsMemoize(int n)
     return stepPermsMemoize(n, cache);
 }
 
+//! @brief Dynamic programming solution from HR discussion section
+//! @param[in] n Number of stairs in the staircase
+//! @return Number of ways Davis can climb staircase
+static int stepPermsDP(int n)
+{
+    //! @details
+    //!     https://www.hackerrank.com/challenges/ctci-recursive-staircase/forum
+
+    std::vector<int> dp(static_cast<std::size_t>(std::max(n + 1, 4)));
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for (int i = 4; i < n + 1; ++i)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+    
+    return dp[n];
+
+} // static int stepPermsDP( ...
+
 // Test case 0
 TEST(StepPermsTest, TestCase0) {
     EXPECT_EQ(1, stepPermsFirstAttempt(1));
@@ -79,6 +103,10 @@ TEST(StepPermsTest, TestCase0) {
     EXPECT_EQ(1, stepPermsMemoize(1));
     EXPECT_EQ(4, stepPermsMemoize(3));
     EXPECT_EQ(44, stepPermsMemoize(7));
+
+    EXPECT_EQ(1, stepPermsDP(1));
+    EXPECT_EQ(4, stepPermsDP(3));
+    EXPECT_EQ(44, stepPermsDP(7));
 }
 
 // Test case 9
@@ -88,6 +116,9 @@ TEST(StepPermsTest, TestCase9) {
 
     EXPECT_EQ(13, stepPermsMemoize(5));
     EXPECT_EQ(81, stepPermsMemoize(8));
+
+    EXPECT_EQ(13, stepPermsDP(5));
+    EXPECT_EQ(81, stepPermsDP(8));
 }
 
 // Test case 10
@@ -99,4 +130,8 @@ TEST(StepPermsTest, TestCase10) {
     EXPECT_EQ(5768, stepPermsMemoize(15));
     EXPECT_EQ(121415, stepPermsMemoize(20));
     EXPECT_EQ(8646064, stepPermsMemoize(27));
+
+    EXPECT_EQ(5768, stepPermsDP(15));
+    EXPECT_EQ(121415, stepPermsDP(20));
+    EXPECT_EQ(8646064, stepPermsDP(27));
 }
