a74da9183705d003250a4646a1c033ce7acfb3d2
Jeffry <jeffryklew@gmail.com>
2022-10-17 21:29:46 -0400

    Implement first discussion solution in cpp/miscellaneous/flipping_bits.cpp

--- cpp/miscellaneous/flipping_bits.cpp
+++ cpp/miscellaneous/flipping_bits.cpp
@@ -20,17 +20,42 @@ static long flippingBits(long n)
     return output;
 }
 
+//! @brief Solution from HR discussion section
+//! @param[in] n Number whose bits needs to be flipped
+//! @return Number with flipped bits as a long
+static long flippingBitsDS1(long n)
+{
+    //! 2^32 - 1 = 4,294,967,296 - 1 = 1111 1111 1111 1111 1111 1111 1111 1111
+    //! Flipping the bits is the same as subtracting from 2^32 - 1
+    //!
+    //! Smaller example:
+    //!   2^4 - 1 = 15 = 1111
+    //!   n = 9 = 1001
+    //!   Flipped n = 0110 = 6
+    //!   1111 - 1001 = 0110
+    //!        15 - 9 = 6
+
+    return static_cast<long>(std::pow(2.0, 32.0)) - 1L - n;
+}
+
 // Test case 0
 TEST(FlippingBitsTest, TestCase0) {
     EXPECT_EQ(2147483648L, flippingBits(2147483647L));
     EXPECT_EQ(4294967294L, flippingBits(1L));
     EXPECT_EQ(4294967295L, flippingBits(0L));
+
+    EXPECT_EQ(2147483648L, flippingBitsDS1(2147483647L));
+    EXPECT_EQ(4294967294L, flippingBitsDS1(1L));
+    EXPECT_EQ(4294967295L, flippingBitsDS1(0L));
 }
 
 // Test case 10
 TEST(FlippingBitsTest, TestCase10) {
     EXPECT_EQ(4294967291L, flippingBits(4L));
     EXPECT_EQ(4294843839L, flippingBits(123456L));
+
+    EXPECT_EQ(4294967291L, flippingBitsDS1(4L));
+    EXPECT_EQ(4294843839L, flippingBitsDS1(123456L));
 }
 
 // Test case 11
@@ -38,4 +63,8 @@ TEST(FlippingBitsTest, TestCase11) {
     EXPECT_EQ(4294967295L, flippingBits(0L));
     EXPECT_EQ(3492223820L, flippingBits(802743475L));
     EXPECT_EQ(4259365872L, flippingBits(35601423L));
+
+    EXPECT_EQ(4294967295L, flippingBitsDS1(0L));
+    EXPECT_EQ(3492223820L, flippingBitsDS1(802743475L));
+    EXPECT_EQ(4259365872L, flippingBitsDS1(35601423L));
 }
