a2378892581874d27951569bc3aaf3b599d4fcb2
Jeffry <jeffryklew@gmail.com>
2022-10-18 17:37:14 -0400

    Implement second discussion solution in cpp/miscellaneous/flipping_bits.cpp

--- cpp/miscellaneous/flipping_bits.cpp
+++ cpp/miscellaneous/flipping_bits.cpp
@@ -25,6 +25,8 @@ static long flippingBits(long n)
 //! @return Number with flipped bits as a long
 static long flippingBitsDS1(long n)
 {
+    //! @details https://www.hackerrank.com/challenges/flipping-bits/forum
+    //!
     //! 2^32 - 1 = 4,294,967,296 - 1 = 1111 1111 1111 1111 1111 1111 1111 1111
     //! Flipping the bits is the same as subtracting from 2^32 - 1
     //!
@@ -38,6 +40,22 @@ static long flippingBitsDS1(long n)
     return static_cast<long>(std::pow(2.0, 32.0)) - 1L - n;
 }
 
+//! @brief Second solution from HR discussion section
+//! @param[in] n Number whose bits needs to be flipped
+//! @return Number with flipped bits as a long
+static long flippingBitsDS2(long n)
+{
+    //! @details https://www.hackerrank.com/challenges/flipping-bits/forum
+    //!
+    //! 2^32 - 1 = 4,294,967,295 = 1111 1111 1111 1111 1111 1111 1111 1111
+    //! Flipping the bits is the same as bitwise XOR with 2^32 - 1
+
+    static const long all_ones {static_cast<long>(std::pow(2.0, 32.0)) - 1L};
+
+    //! ^ = bitwise XOR
+    return n ^ all_ones;
+}
+
 // Test case 0
 TEST(FlippingBitsTest, TestCase0) {
     EXPECT_EQ(2147483648L, flippingBits(2147483647L));
@@ -47,6 +65,10 @@ TEST(FlippingBitsTest, TestCase0) {
     EXPECT_EQ(2147483648L, flippingBitsDS1(2147483647L));
     EXPECT_EQ(4294967294L, flippingBitsDS1(1L));
     EXPECT_EQ(4294967295L, flippingBitsDS1(0L));
+
+    EXPECT_EQ(2147483648L, flippingBitsDS2(2147483647L));
+    EXPECT_EQ(4294967294L, flippingBitsDS2(1L));
+    EXPECT_EQ(4294967295L, flippingBitsDS2(0L));
 }
 
 // Test case 10
@@ -56,6 +78,9 @@ TEST(FlippingBitsTest, TestCase10) {
 
     EXPECT_EQ(4294967291L, flippingBitsDS1(4L));
     EXPECT_EQ(4294843839L, flippingBitsDS1(123456L));
+
+    EXPECT_EQ(4294967291L, flippingBitsDS2(4L));
+    EXPECT_EQ(4294843839L, flippingBitsDS2(123456L));
 }
 
 // Test case 11
@@ -67,4 +92,8 @@ TEST(FlippingBitsTest, TestCase11) {
     EXPECT_EQ(4294967295L, flippingBitsDS1(0L));
     EXPECT_EQ(3492223820L, flippingBitsDS1(802743475L));
     EXPECT_EQ(4259365872L, flippingBitsDS1(35601423L));
+
+    EXPECT_EQ(4294967295L, flippingBitsDS2(0L));
+    EXPECT_EQ(3492223820L, flippingBitsDS2(802743475L));
+    EXPECT_EQ(4259365872L, flippingBitsDS2(35601423L));
 }
