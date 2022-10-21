741c516c97f2406cf999742ee633a1aba0cdb7b8
Jeffry <jeffryklew@gmail.com>
2022-10-21 17:14:27 -0400

    Implement discussion solution in cpp/algorithms/plus_minus.cpp

--- cpp/algorithms/plus_minus.cpp
+++ cpp/algorithms/plus_minus.cpp
@@ -40,6 +40,31 @@ static std::tuple<std::string, std::string, std::string> plusMinus(
 
 } // static std::tuple<std::string, std::string, std::string> plusMinus( ...
 
+//! @brief Less efficient solution from HR discussion section O(3n) time
+//! @param[in] arr Vector of integers
+//! @return Strings for positive ratio, negative ratio, and zero ratio
+static std::tuple<std::string, std::string, std::string> plusMinusDS(
+    std::vector<int> arr)
+{
+    const auto arr_size = static_cast<double>(arr.size());
+
+    const auto num_pos = std::count_if(arr.cbegin(),
+                                       arr.cend(),
+                                       [](int ele) -> bool {
+                                           return ele > 0;
+                                       });
+    const auto num_neg = std::count_if(arr.cbegin(),
+                                       arr.cend(),
+                                       [](int ele) -> bool {
+                                           return ele < 0;
+                                       });
+    const auto num_zero = std::count(arr.cbegin(), arr.cend(), 0);
+
+    return std::make_tuple(std::to_string(num_pos / arr_size),
+                           std::to_string(num_neg / arr_size),
+                           std::to_string(num_zero / arr_size));
+}
+
 // Test case 1
 TEST(PlusMinusTest, TestCase1) {
     const std::string positive_ratio {"0.500000"};
@@ -48,6 +73,9 @@ TEST(PlusMinusTest, TestCase1) {
 
     EXPECT_EQ(std::tie(positive_ratio, negative_ratio, zero_ratio),
               plusMinus({-4, 3, -9, 0, 4, 1}));
+
+    EXPECT_EQ(std::tie(positive_ratio, negative_ratio, zero_ratio),
+              plusMinusDS({-4, 3, -9, 0, 4, 1}));
 }
 
 // Test case 11
@@ -58,4 +86,7 @@ TEST(PlusMinusTest, TestCase11) {
 
     EXPECT_EQ(std::tie(positive_ratio, negative_ratio, zero_ratio),
               plusMinus({1, 2, 3, -1, -2, -3, 0, 0}));
+
+    EXPECT_EQ(std::tie(positive_ratio, negative_ratio, zero_ratio),
+              plusMinusDS({1, 2, 3, -1, -2, -3, 0, 0}));
 }
