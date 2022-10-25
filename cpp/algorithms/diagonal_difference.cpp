56fe7d9ae003e2922c04ce22eea5d786bfbc907f
Jeffry <jeffryklew@gmail.com>
2022-10-24 20:50:00 -0400

    Implement cpp/algorithms/diagonal_difference.cpp

---
+++ cpp/algorithms/diagonal_difference.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+#include <cmath>
+#include <vector>
+
+//! @brief Calculate abs diff between sums of its diagonals
+//! @param[in] arr 2D vector of integers
+//! @return The absolute diagonal difference
+static int diagonalDifference(std::vector<std::vector<int>> arr)
+{
+    const auto arr_size = static_cast<int>(arr.size());
+    
+    //! Evaluate left-to-right and right-to-left diagonals
+    int col {arr_size};
+    int ltr_diagonal {};
+    int rtl_diagonal {};
+    for (int i = 0; i < arr_size; ++i)
+    {
+        ltr_diagonal += arr[i][i];
+        rtl_diagonal += arr[i][--col];
+    }
+    
+    return std::abs(ltr_diagonal - rtl_diagonal);
+
+} // static int diagonalDifference( ...
+
+// Test case 0
+TEST(DiagonalDifferenceTest, TestCase0) {
+    EXPECT_EQ(15, diagonalDifference({{11, 2, 4}, {4, 5, 6}, {10, 8, -12}}));
+}
