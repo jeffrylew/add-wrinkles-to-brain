37fee6106a064aeb0d8723ba51c4e81748caa8d3
Jeffry <jeffryklew@gmail.com>
2022-10-25 21:52:12 -0400

    Implement discussion solution in cpp/algorithms/diagonal_difference.cpp

--- cpp/algorithms/diagonal_difference.cpp
+++ cpp/algorithms/diagonal_difference.cpp
@@ -24,7 +24,38 @@ static int diagonalDifference(std::vector<std::vector<int>> arr)
 
 } // static int diagonalDifference( ...
 
+//! @brief Solution from HR discussion section
+//! @param[in] arr 2D vector of integers
+//! @return The absolute diagonal difference
+static int diagonalDifferenceDS(std::vector<std::vector<int>> arr)
+{
+    //! Square matrix
+    const auto arr_size = static_cast<int>(arr.size());
+
+    int leftD {};
+    int rightD {};
+
+    for (int j = 0; j < arr_size; ++j)
+    {
+        for (int i = 0; i < arr_size; ++i)
+        {
+            if (j == i)
+            {
+                leftD += arr[j][i];
+            }
+            if (j + i == (arr_size - 1))
+            {
+                rightD += arr[j][i];
+            }
+        }
+    }
+    
+    return std::abs(leftD - rightD);
+
+} // static int diagonalDifferenceDS( ...
+
 // Test case 0
 TEST(DiagonalDifferenceTest, TestCase0) {
     EXPECT_EQ(15, diagonalDifference({{11, 2, 4}, {4, 5, 6}, {10, 8, -12}}));
+    EXPECT_EQ(15, diagonalDifferenceDS({{11, 2, 4}, {4, 5, 6}, {10, 8, -12}}));
 }
