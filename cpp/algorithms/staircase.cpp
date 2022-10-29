333fdf408be1f0facfd2c753ad88883d01eeaca0
Jeffry <jeffryklew@gmail.com>
2022-10-29 09:18:07 -0400

    Implement discussion solution in cpp/algorithms/staircase.cpp

--- cpp/algorithms/staircase.cpp
+++ cpp/algorithms/staircase.cpp
@@ -28,10 +28,29 @@ static std::string staircase(int n)
 
 } // static std::string staircase(int n)
 
+//! @brief Solution from HR discussion section
+//! @param[in] n Size of staircase
+//! @return std::string containing staircase of size n
+static std::string staircaseDS(int n)
+{
+    std::string output {};
+
+    for (int i = 1; i <= n; ++i)
+    {
+        const std::string spaces(n - i, ' ');
+        const std::string pounds(i, '#');
+        output += spaces + pounds + "\n";
+    }
+
+    return output;
+    
+} // static std::string staircaseDS(int n)
+
 // Test case 1
 TEST(StaircaseTest, TestCase1) {
     const std::string expected {
         "     #\n    ##\n   ###\n  ####\n #####\n######\n"};
 
     EXPECT_EQ(expected, staircase(6));
+    EXPECT_EQ(expected, staircaseDS(6));
 }
