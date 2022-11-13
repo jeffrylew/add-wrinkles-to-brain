1719fdf8aa9a266ea6e4ca4561fe2739499fb7e6
Jeffry <jeffryklew@gmail.com>
2022-11-12 20:02:46 -0500

    Implement discussion solution in cpp/algorithms/mini_max_sum.cpp

--- cpp/algorithms/mini_max_sum.cpp
+++ cpp/algorithms/mini_max_sum.cpp
@@ -2,6 +2,8 @@
 
 #include <algorithm>
 #include <iterator>
+#include <limits>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -17,12 +19,39 @@ static std::string miniMaxSum(std::vector<int> arr)
                                             0LL));
 }
 
+//! @brief Solution from HR discussion section
+//! @param[in] arr Vector of five integers
+//! @return std::string containing min and max sums of 4 of 5 elements
+static std::string miniMaxSumDS(std::vector<int> arr)
+{
+    long min {std::numeric_limits<long>::max()};
+    long max {};
+    long sum {};
+
+    for (const auto ele : arr)
+    {
+        sum += ele;
+        if (ele < min)
+        {
+            min = ele;
+        }
+        if (ele > max)
+        {
+            max = ele;
+        }
+    }
+
+    return std::to_string(sum - max) + " " + std::to_string(sum - min);
+}
+
 // Test case 0
 TEST(MiniMaxSumTest, TestCase0) {
     EXPECT_EQ("10 14", miniMaxSum({1, 2, 3, 4, 5}));
+    EXPECT_EQ("10 14", miniMaxSumDS({1, 2, 3, 4, 5}));
 }
 
 // Test case 14
 TEST(MiniMaxSumTest, TestCase14) {
     EXPECT_EQ("299 9271", miniMaxSum({7, 69, 2, 221, 8974}));
+    EXPECT_EQ("299 9271", miniMaxSumDS({7, 69, 2, 221, 8974}));
 }
