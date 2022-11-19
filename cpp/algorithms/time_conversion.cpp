ed6ce0e844c9d576cca7b1428483712c6f838e30
Jeffry <jeffryklew@gmail.com>
2022-11-19 07:40:55 -0500

    Implement second discussion solution in time_conversion.cpp

--- cpp/algorithms/time_conversion.cpp
+++ cpp/algorithms/time_conversion.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 
+#include <cstdio>
 #include <sstream>
 #include <string>
+#include <tuple>
 
 static std::string timeConversion(std::string s)
 {
@@ -34,6 +36,8 @@ static std::string timeConversion(std::string s)
 //! @return std::string containing time in 24 hour format
 static std::string timeConversionDS(std::string s)
 {
+    //! @details https://www.hackerrank.com/challenges/time-conversion/forum
+
     std::stringstream ss {s};
     std::string       ele {};
 
@@ -52,8 +56,50 @@ static std::string timeConversionDS(std::string s)
 
 } // static std::string timeConversionDS( ...
 
+//! @brief Second solution inspired by HR discussion section
+//! @param[in] s std::string containing time in 12 hour format
+//! @return std::string containing time in 24 hour format
+static std::string timeConversionDS2(std::string s)
+{
+    //! @details https://www.hackerrank.com/challenges/time-conversion/forum
+
+    int hh {};
+    int mm {};
+    int ss {};
+
+    std::string t12(3ULL, '\0');
+
+    std::ignore = std::sscanf(s.c_str(), "%d:%d:%d%s", &hh, &mm, &ss, &t12[0]);
+
+    if (t12.substr(0ULL, 2ULL) == "PM" && hh != 12)
+    {
+        hh += 12;
+    }
+
+    if (t12.substr(0ULL, 2ULL) == "AM" && hh == 12)
+    {
+        hh = 0;
+    }
+    
+    //! Length of output string including null char
+    constexpr std::size_t military_t_len {9ULL};
+    std::string           output(military_t_len, '\0');
+
+    std::ignore = std::snprintf(&output[0],
+                                military_t_len,
+                                "%02d:%02d:%02d",
+                                hh,
+                                mm,
+                                ss);
+    
+    //! Exclude null terminator
+    return output.substr(0ULL, military_t_len - 1ULL);
+
+} // static std:string timeConversionDS2( ...
+
 // Test case 0
 TEST(TimeConversionTest, TestCase0) {
     EXPECT_EQ("19:05:45", timeConversion("07:05:45PM"));
     EXPECT_EQ("19:05:45", timeConversionDS("07:05:45PM"));
+    EXPECT_EQ("19:05:45", timeConversionDS2("07:05:45PM"));
 }
