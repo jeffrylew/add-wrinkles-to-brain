73bc58f693d3d080d4f092815abfb5e422910f8d
Jeffry <jeffryklew@gmail.com>
2022-11-23 23:44:47 -0500

    Add cpp/stacks_and_queues/castle_on_the_grid.cpp skeleton

---
+++ cpp/stacks_and_queues/castle_on_the_grid.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
+
+//! @brief First attempt solution
+//! @param[in] grid   Vector of strings representing rows of grid
+//! @param[in] startX Starting X coordinate
+//! @param[in] startY Starting Y coordinate
+//! @param[in] goalX  Ending X coordinate
+//! @param[in] goalY  Ending Y coordinate
+//! @return Minimum moves to reach the goal
+static int minimumMovesFA(std::vector<std::string> grid,
+                          int                      startX,
+                          int                      startY,
+                          int                      goalX,
+                          int                      goalY)
+{
+    //! @todo
+
+} // static int minimumMovesFA( ...
+
+// Test case X
+TEST(MinimumMovesTest, TestCaseX) {
+    //! @todo
+}
