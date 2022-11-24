d0ffc87681af0e2b6ad958f8081f92db631d1f38
Jeffry <jeffryklew@gmail.com>
2022-11-24 10:52:33 -0500

    Implement first attempt solution in castle_on_the_grid.cpp

--- cpp/stacks_and_queues/castle_on_the_grid.cpp
+++ cpp/stacks_and_queues/castle_on_the_grid.cpp
@@ -2,10 +2,10 @@
 
 #include <queue>
 #include <string>
-#include <utility>
+#include <tuple>
 #include <vector>
 
-//! @brief First attempt solution
+//! @brief First attempt solution passes test cases 10, 12, 13
 //! @param[in] grid   Vector of strings representing rows of grid
 //! @param[in] startX Starting X coordinate
 //! @param[in] startY Starting Y coordinate
@@ -18,11 +18,96 @@ static int minimumMovesFA(std::vector<std::string> grid,
                           int                      goalX,
                           int                      goalY)
 {
-    //! @todo
+    //! Enum indicating if at start coord, traveling along row,
+    //! or traveling along col
+    enum class Dir
+    {
+        start,
+        travel_row,
+        travel_col
+    };
+    
+    //! Queue of coords/data <row, col, Dir, moves> to process
+    std::queue<std::tuple<int, int, Dir, int>> unexplored_coords {};
+    unexplored_coords.emplace(startX, startY, Dir::start, 0);
+    
+    //! Minimum number of moves
+    int nmoves {};
+    
+    //! Grid extents
+    const auto nrows = static_cast<int>(grid.size());
+    const auto ncols = static_cast<int>(grid.front().size());
+    
+    while (not unexplored_coords.empty())
+    {
+        const auto data = unexplored_coords.front();
+        unexplored_coords.pop();
+        
+        //! Current coordinate/direction/number of moves
+        const auto row   = std::get<0>(data);
+        const auto col   = std::get<1>(data);
+        const auto dir   = std::get<2>(data);
+        const auto moves = std::get<3>(data);
+        
+        if (row == goalX && col == goalY)
+        {
+            return moves;
+        }
+    
+        //! Add valid adjacent col (Y) coordinates if not 'X'
+        if (col - 1 >= 0 && grid[row][col - 1] != 'X')
+        {
+            if (dir != Dir::travel_row)
+            {
+                nmoves = moves + 1;
+            }
+            unexplored_coords.emplace(row, col - 1, Dir::travel_row, nmoves);
+        }
+        if (col + 1 < ncols && grid[row][col + 1] != 'X')
+        {
+            if (dir != Dir::travel_row)
+            {
+                nmoves = moves + 1;
+            }
+            unexplored_coords.emplace(row, col + 1, Dir::travel_row, nmoves);
+        }
+        
+        //! Add valid adjacent row (X) coordinates if not 'X'
+        if (row - 1 >= 0 && grid[row - 1][col] != 'X')
+        {
+            if (dir != Dir::travel_col)
+            {
+                nmoves = moves + 1;
+            }
+            unexplored_coords.emplace(row - 1, col, Dir::travel_col, nmoves);
+        }
+        if (row + 1 < nrows && grid[row + 1][col] != 'X')
+        {
+            if (dir != Dir::travel_col)
+            {
+                nmoves = moves + 1;
+            }
+            unexplored_coords.emplace(row + 1, col, Dir::travel_col, nmoves);
+        }
+        
+    } // while (not unexplored_coords.empty())
+    
+    //! If this point is reached then goal cannot be reached from start
+    return 0;
 
 } // static int minimumMovesFA( ...
 
-// Test case X
-TEST(MinimumMovesTest, TestCaseX) {
-    //! @todo
+// Test case 10
+TEST(MinimumMovesTest, TestCase10) {
+    EXPECT_EQ(3, minimumMovesFA({".X.", ".X.", "..."}, 0, 0, 0, 2));
+}
+
+// Test case 12
+TEST(MinimumMovesTest, TestCase12) {
+    EXPECT_EQ(2, minimumMovesFA({"...", ".X.", ".X."}, 2, 0, 0, 2));
+}
+
+// Test case 13
+TEST(MinimumMovesTest, TestCase13) {
+    EXPECT_EQ(3, minimumMovesFA({"...", ".X.", ".X."}, 2, 0, 2, 2));
 }
