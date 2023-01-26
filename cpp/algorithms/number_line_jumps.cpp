9cceedf7a2b9d3b15135eb6739e6888c1e5d5310
Jeffry <jeffryklew@gmail.com>
2023-01-25 20:24:23 -0500

    Implement discussion solution in cpp/algorithms/number_line_jumps.cpp

--- cpp/algorithms/number_line_jumps.cpp
+++ cpp/algorithms/number_line_jumps.cpp
@@ -34,14 +34,37 @@ static std::string kangarooSA(int x1, int v1, int x2, int v2)
     return 1.0 * (x1 - x2) / (v2 - v1) < 1.0 ? "NO" : "YES";
 }
 
+//! @brief Solution from HR discussion section
+//! @param[in] x1 Starting location of first kangaroo
+//! @param[in] v1 Distance covered per jump of first kangaroo
+//! @param[in] x2 Starting location of second kangaroo
+//! @param[in] v2 Distance covered per jump of second kangaroo
+//! @return "YES" if both kangaroos can reach same location, else no
+static std::string kangarooDS(int x1, int v1, int x2, int v2)
+{
+    //! @details https://www.hackerrank.com/challenges/kangaroo/forum
+
+    const bool canCatchUp {v2 < v1};
+
+    if (canCatchUp)
+    {
+        const bool willIntersect {(x1 - x2) % (v2 - v1) == 0};
+        return willIntersect ? "YES" : "NO";
+    }
+
+    return "NO";
+}
+
 // Test case 0
 TEST(KangarooTest, TestCase0) {
     EXPECT_EQ("YES", kangarooFA(0, 3, 4, 2));
     EXPECT_EQ("YES", kangarooSA(0, 3, 4, 2));
+    EXPECT_EQ("YES", kangarooDS(0, 3, 4, 2));
 }
 
 // Test case 1
 TEST(KangarooTest, TestCase1) {
     EXPECT_EQ("NO", kangarooFA(0, 2, 5, 3));
     EXPECT_EQ("NO", kangarooSA(0, 2, 5, 3));
+    EXPECT_EQ("NO", kangarooDS(0, 2, 5, 3));
 }
